5a99fba097c15c9eeb2b3ce620dafb77de6e960b
fire-irror <firenoah.m@gmail.com>
2024-05-11 00:30:43 +0900

    Fix : 로그인 화면 완성

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import Logo from '../../assets/Logo.svg'
 import styles from '../../css/auth/Login.module.css'
+import socal from '../../assets/socal.svg'
 
 
 export default function Login() {
+  const navigate = useNavigate()
+
+  const hanldeJoinUpText = () =>{
+    navigate('/join')
+  }
 
   return (
     <div className={styles.wrap}>
@@ -24,9 +31,14 @@ export default function Login() {
         </div>
 
         <button className={styles.LoginBtn}>로그인</button>
-        
+
       </div>
       <p className={styles.socalLoginText}>- on sign in with -</p>
+      <img src={socal} className={styles.socalImg} />
+      <div className={styles.wrapSocal}>
+        <p className={styles.AlreadyText}>Already have an Account?</p>
+        <p className={styles.JoinUpTtext} onClick={hanldeJoinUpText}>Sign up</p>
+      </div>
     </div>
   )
 }
