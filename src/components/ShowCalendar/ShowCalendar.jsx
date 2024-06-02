2277ab0faff7137de58826dfb753523b8c4282a4
fire-irror <firenoah.m@gmail.com>
2024-06-03 01:36:24 +0900

    Fix : 캘린더 css 수정 중

--- src/components/ShowCalendar/ShowCalendar.jsx
+++ src/components/ShowCalendar/ShowCalendar.jsx
@@ -1,13 +1,39 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'; // css import
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+import '../../css/showCalendar/showCalendar.css';
 
 export default function ShowCalendar() {
-  const [value, onChange] = useState(new Date());
+  const today = new Date();
+  const [date, setDate] = useState(today);
+  const [activeStartDate, setActiveStartDate] = useState(new Date());
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+  };
+
+  const handleTodayClick = () => {
+    const today = new Date();
+    setActiveStartDate(today);
+    setDate(today);
+  };
 
   return (
-    <div>
-      <Calendar onChange={onChange} value={value} />
+    <div className='container'>
+      <Calendar
+        value={date}
+        onChange={handleDateChange}
+        formatDay={(locale, date) => moment(date).format("D")}
+        formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
+        calendarType="gregory"
+        showNeighboringMonth={false}
+        next2Label={null}
+        prev2Label={null}
+        minDetail="year"
+        className='reactCalendar'
+      />
+      <button onClick={handleTodayClick} className='todayButton'>오늘</button>
     </div>
   );
 }
