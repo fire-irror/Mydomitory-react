287b8b4185e9a7ca2cc555e7fd0d2ecd83f428d2
fire-irror <firenoah.m@gmail.com>
2024-06-03 22:38:43 +0900

    Fix : 앞으로 가기 버튼 추가

--- src/components/ShowCalendar/ShowCalendar.jsx
+++ src/components/ShowCalendar/ShowCalendar.jsx
@@ -4,10 +4,13 @@ import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
 import '../../css/showCalendar/showCalendar.css';
 import Schedule from './Schedule';
+import back from '../../assets/backBtn.svg';
+import { useNavigate } from 'react-router';
 
 export default function ShowCalendar() {
   const today = new Date();
   const [date, setDate] = useState(today);
+  const nav = useNavigate();
 
   const handleDateChange = (newDate) => {
     setDate(newDate);
@@ -16,28 +19,36 @@ export default function ShowCalendar() {
   const formattedDate = moment(date).format('YYYY.MM.DD');
   const dayOfWeek = moment(date).format('dddd');
 
+  const showCalendarBackbtn = () => {
+    nav(-1);
+  };
+
   return (
-    <div className='container'>
-      <Schedule />
+    <div>
       <div className='ShowCalendarWrap'>
+        <img src={back} className='showCalendarBackbtn' onClick={showCalendarBackbtn} />
         <p className="showCalendarTitle">HOME</p>
       </div>
-      <Calendar
-        value={date}
-        onChange={handleDateChange}
-        formatDay={(locale, date) => moment(date).format("D")}
-        formatMonthYear={(locale, date) => moment(date).format("YYYY.MM")}
-        calendarType="gregory"
-        showNeighboringMonth={false}
-        next2Label={null}
-        prev2Label={null}
-        minDetail="year"
-        className='reactCalendar'
-      />
-      <div className="todayInfoContainer">
-        <div className="todayInfoBox">
-          <p className="todayDayOfWeek">{dayOfWeek}</p>
-          <p className="todayDate">{formattedDate}</p>
+
+      <div className='container'>
+        <Schedule />
+        <Calendar
+          value={date}
+          onChange={handleDateChange}
+          formatDay={(locale, date) => moment(date).format("D")}
+          formatMonthYear={(locale, date) => moment(date).format("YYYY.MM")}
+          calendarType="gregory"
+          showNeighboringMonth={false}
+          next2Label={null}
+          prev2Label={null}
+          minDetail="year"
+          className='reactCalendar'
+        />
+        <div className="todayInfoContainer">
+          <div className="todayInfoBox">
+            <p className="todayDayOfWeek">{dayOfWeek}</p>
+            <p className="todayDate">{formattedDate}</p>
+          </div>
         </div>
       </div>
     </div>
